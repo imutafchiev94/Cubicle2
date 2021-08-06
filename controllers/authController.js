20f40d83b38cd192b2156fecbf9617a08eace4e0
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-08-06 13:46:00 +0300

    add validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ const {Router} = require('express');
 
 const {COOKIE_NAME} = require('../config/config');
 const authService = require('../services/authService');
+const validator = require('validator');
+const {check, validationResult} = require('express-validator');
+
 const isGuest = require('../middlewares/isGuest');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 
@@ -26,12 +29,25 @@ router.post('/login', isGuest, async (req, res) => {
 
 router.post('/register', isGuest, async (req, res) => {
     const {username, password, repeatPassword} = req.body;
+    let isStrong = validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+    });
+
+    
 
     if(password !== repeatPassword) {
        return res.render('register', {message: 'Password missmatch'});
     }
 
     try {
+        if(!isStrong) {
+            return res.render('register', {error: { message: "You should have strong password"}, username: req.body.username});
+        }
+
         let result = await authService.register(req.body);
 
         console.log(result);
